19e48a56d4362e95b693eec9700ddac7c7af4154
PRAJWAL K K <prajwal11kk22@gmail.com>
2025-12-10 15:48:17 +0530

    Fix static file directory path in production server

--- server/index-prod.ts
+++ server/index-prod.ts
@@ -6,7 +6,7 @@ import express, { type Express } from "express";
 import runApp from "./app";
 
 export async function serveStatic(app: Express, _server: Server) {
-  const distPath = path.resolve(import.meta.dirname, "public");
+  const distPath = path.resolve(import.meta.dirname, "../public");
 
   if (!fs.existsSync(distPath)) {
     throw new Error(
@@ -16,7 +16,6 @@ export async function serveStatic(app: Express, _server: Server) {
 
   app.use(express.static(distPath));
 
-  // fall through to index.html if the file doesn't exist
   app.use("*", (_req, res) => {
     res.sendFile(path.resolve(distPath, "index.html"));
   });
